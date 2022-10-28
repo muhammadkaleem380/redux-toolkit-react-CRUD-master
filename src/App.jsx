a94626c0aa0e96ce8221ee47ad10e10e41c1fcd8
muhammadkaleem380 <116882363+muhammadkaleem380@users.noreply.github.com>
2022-10-28 05:48:40 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Spin } from "antd";
+
+function App() {
+  return (
+    <div>
+      App
+      <Spin size="large" />
+    </div>
+  );
+}
+
+export default App;
